4df145a8c5ae5ab1e469dac64c6cdfd0fd6b60c6
s1081702 <s1081702@gm.pu.edu.tw>
2021-05-21 14:51:53 +0800

    2021上機考v2_(陳佳純)

--- app/src/main/java/tw/edu/pu/s1081702/crazyshape/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/s1081702/crazyshape/MainActivity.kt
@@ -4,8 +4,14 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
 import android.widget.Toast
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
 import kotlinx.android.synthetic.main.activity_main.*
+@GlideModule
+public final class MyAppGlideModule : AppGlideModule()
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,5 +27,9 @@ class MainActivity : AppCompatActivity() {
                 return true
             }
         })
+        val img: ImageView = findViewById(R.id.imgTitle)
+        GlideApp.with(this)
+            .load(R.drawable.cover)
+            .into(img)
     }
 }
